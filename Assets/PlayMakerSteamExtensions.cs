25ca75416e3da1e95ff65d5ee1522ee582738bad
Jean Fabre <contact@fabrejean.net>
2018-12-11 13:09:15 +0300

    example on how to extend a struct

---
+++ Assets/PlayMakerSteamExtensions.cs
@@ -0,0 +1,10 @@
+﻿
+using Steamworks;
+
+public static class PlayMakerSteamExtensions
+{
+    public static int m_ControllerActionSetHandleAsInt(this ControllerActionSetHandle_t st)
+    {
+        return (int)st.m_ControllerActionSetHandle;
+    }
+}
